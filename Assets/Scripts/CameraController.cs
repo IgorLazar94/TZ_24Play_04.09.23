491c982d85f0b0884ecf340e74a2f73bcc42e0b7
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-09-04 14:47:53 +0300

    Create camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private PlayerController target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(offset.x, offset.y, target.transform.position.z + offset.z);
+        }
+    }
+}
