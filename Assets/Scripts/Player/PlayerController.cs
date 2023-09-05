5f53672cff11719a57ea63ddc198e7e10391ae04
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-09-05 16:09:04 +0300

    Add scores text and create animation clip for scores text

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,14 +12,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private BoxCollider[] ragdollBoxColliders;
     [SerializeField] private CapsuleCollider mainCapsule;
     [SerializeField] private Rigidbody mainRigidbody;
+    [SerializeField] private Animation scoresTextAnimation;
     private Rigidbody[] ragdollRigidbodies;
     private float cubeHeight;
     private Animator stickmanAnimator;
     private float pushForceValue;
+    private TextMeshProUGUI scoresText;
 
     private void Start()
     {
         pushForceValue = GameSettings.Instance.GetPlayerPushForceValue();
+        scoresText = GetComponentInChildren<TextMeshProUGUI>();
+        scoresText.gameObject.SetActive(false);
         stickmanAnimator = stickman.GetComponent<Animator>();
         ragdollRigidbodies = stickmanAnimator.GetComponentsInChildren<Rigidbody>();
         cubeHeight = cubeHolderManager.GetComponentInChildren<BoxCollider>().size.y + 1f;
@@ -26,12 +32,14 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         CubeHolderManager.OnAddNewCube += UpdateStickmanHeight;
+        CubeHolderManager.OnAddNewCube += UpdateScoresText;
         GameManager.OnLosingTheGame += EnableRagdoll;
     }
 
     private void OnDisable()
     {
         CubeHolderManager.OnAddNewCube -= UpdateStickmanHeight;
+        CubeHolderManager.OnAddNewCube -= UpdateScoresText;
         GameManager.OnLosingTheGame -= EnableRagdoll;
     }
 
@@ -60,5 +68,23 @@ public class PlayerController : MonoBehaviour
         mainCapsule.enabled = false;
         mainRigidbody.isKinematic = true;
         stickmanAnimator.enabled = false;
+    }
+
+    private void UpdateScoresText()
+    {
+        scoresTextAnimation.gameObject.SetActive(true);
+        scoresTextAnimation.Play();
+        Invoke(nameof(DisableScoresTextAnimation), scoresTextAnimation.clip.length);
+    }
+
+    private void DisableScoresText(Vector2 initialPosition, RectTransform scoresRectTransform)
+    {
+        scoresRectTransform.DOAnchorPosY(initialPosition.y, 0f);
+        scoresText.gameObject.SetActive(false);
+    }
+
+    private void DisableScoresTextAnimation()
+    {
+        scoresTextAnimation.gameObject.SetActive(false);
     }
 }
